24efa48cc7307382c986db02440ca6868c4ca438
anesh1234 <112165922+anesh1234@users.noreply.github.com>
2023-09-25 15:13:22 +0200

    changes

--- miller_rabin_algorithm.py
+++ miller_rabin_algorithm.py
@@ -2,31 +2,31 @@ import random
 
 def miller_rabin(n, k):
 
-    # Write n as 2^s * d + 1
-    s, d = 0, n - 1
+    # Set n − 1 = 2^{s}d where s > 0 and odd number d > 0 
+    s = 0 
+    d = n - 1
+    
     while d % 2 == 0:
         s += 1
         d //= 2
 
     # Perform testing rounds
-    for _ in range(k):
+    for _ in range(1,k):
         a = random.randint(2, n - 2)
         x = pow(a, d, n)
 
-        if x == 1 or x == (n - 1):
-            continue
+        for _ in range(1,s):
+            y = pow(x, 2, n)
+            if (y == 1) and (x != 1) and (x != (n - 1)):
+                return False
+            x = y
 
-        for _ in range(s - 1):
-            x = pow(x, 2, n)
-            if x == (n - 1):
-                break
-        else:
-            return False  # Definitely composite
-
-    return True  # Likely prime
+        if (y != 1):
+            return False
+    return True
 
 # Example usage:
-n = 37
+n = 13
 k = 5
 is_prime = miller_rabin(n, k)
 print(f"{n} is {'probably prime' if is_prime else 'composite'}")
