004d9034055654531a260cdc29b59e19eec1a2e6
anesh1234 <112165922+anesh1234@users.noreply.github.com>
2023-09-25 13:14:54 +0200

    changes

--- pollards_rho_algorithm.py
+++ pollards_rho_algorithm.py
@@ -27,14 +27,22 @@ def pollard_rho(n, k):
 # Using the Algorithm:
 p1 = 123456789
 p2 = 123456789
+bool1 = False
+bool2 = False
 
 factor_p11 = pollard_rho(p1, 2)   # Calls the algorithm with the prime and a start value for the tortoise-and-hare-numbers, to find the non-trivial factor p
 factor_p12 = p1 // factor_p11      # Calculates the other factor, q
+if (factor_p11 * factor_p12) == p1:  # Verifying the result
+    bool1 = True
 
 factor_p21 = pollard_rho(p2, 2)
 factor_p22 = p2 // factor_p21
+if (factor_p21 * factor_p22) == p2:
+    bool2 = True
 
+# Printing the results to terminal
 print("\nA factor of p1 is: ", factor_p11)
-print("Which gives p1q1 = ", factor_p11, "*", factor_p12, "=", p1)
+print("Which gives p1q1 = ", factor_p11, "*", factor_p12, "=", p1, "=", bool1)
+
 print("\nA factor of p2 is: ", factor_p21)
-print("Which gives p2q2 = ", factor_p21, "*", factor_p22, "=", p2)
+print("Which gives p2q2 = ", factor_p21, "*", factor_p22, "=", p2, "=", bool2)
