fec57569ef9ddbec5e1d87dbc180108b830dce84
anesh1234 <112165922+anesh1234@users.noreply.github.com>
2023-09-27 16:36:46 +0200

    added prime generator

--- miller_rabin_algorithm.py
+++ miller_rabin_algorithm.py
@@ -1,21 +1,34 @@
 import random
 
-def miller_rabin(n, k):
+def miller_rabin(b, k):
+    while True:
+        print("første while")
+        randint = random.randint(2**(b-1), 2**b - 1)
+        if (randint % 2) != 0:
+            if prime_test(randint, k):
+                return randint
+
+def prime_test(n, k):
 
     # Set n − 1 = 2^{s}d where s > 0 and odd number d > 0 
-    s = 0 
-    d = n - 1
-    
-    while d % 2 == 0:
-        s += 1
-        d //= 2
+    s = n - 1
+    d = 1
+    while s % 2 == 0:
+        print("andre while")
+        s //= 2
 
     # Perform testing rounds
     for _ in range(k):
+
+        print("range k")
+
         a = random.randint(2, n - 2)   #Generate a random integer from 2 to n-2 inclusive
         x = pow(a, d, n)
-
+        y = 0
         for _ in range(s):
+
+            print("range s")
+
             y = pow(x, 2, n)
             if (y == 1) and (x != 1) and (x != (n - 1)):
                 return False
@@ -25,13 +38,19 @@ def miller_rabin(n, k):
             return False
     return True
 
-# Using the algorithm:
-n = 13236194115964104703902862415812148871696282343519140367937221479351974709349661452569263794914812294244750567811545456611019007622776285166768397975112867
-n2 = 11558395541614906973213701641967786161216081332381300761058031206021381967945981620438995924467438386705513165559014974225617034126770238633671052125770027
+
+# Finding a 512 bit number and testing its primality :
+b = 512
 k = 1000
 
-is_prime = miller_rabin(n, k)
-print(f"\nThe number \n{n} \nis {'probably prime' if is_prime else 'Composite'}")
+n = miller_rabin(b, k)
 
-is_prime = miller_rabin(n2, k)
-print(f"\nThe number \n{n2} \nis {'probably prime' if is_prime else 'composite'}")
+# Finding a seccond number and ensuring that the two numbers are not the same
+while True:
+    print("tredje while")
+    n2 = miller_rabin(b,k)
+    if n != n2:
+        break
+
+print(f"\nThe number \n{n} \nis probably prime")
+print(f"\nThe number \n{n2} \nis another probable prime")
